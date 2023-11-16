a996d6265e5860b2f97d4f1d37b68d630c46ab8c
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-11-16 15:57:51 -0600

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+import ImageList from "./components/ImageList";
 
 const App = () => {
-  return <div>App</div>;
+  const [images, setImages] = useState([]);
+  const handleSumit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={handleSumit} />
+      <ImageList images={images} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,24 @@
-import React from "react";
+import "./SearchBar.css";
+import React, { useState } from "react";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+    setTerm("");
+  };
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="input">Enter Search Term</label>
+        <input type="text" value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
